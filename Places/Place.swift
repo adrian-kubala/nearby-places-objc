a3b72d254f5519044d9d9d3ac2aa89e35925754f
Adrian <adrian.kubala@nextisgreat.com>
2016-09-15 08:09:05 +0200

    Initial commit

---
+++ Places/Place.swift
@@ -0,0 +1,14 @@
+//
+//  Place.swift
+//  Places
+//
+//  Created by Adrian on 15.09.2016.
+//  Copyright © 2016 Adrian Kubała. All rights reserved.
+//
+
+import Foundation
+
+class Place {
+    let longitude = 54.189971
+    let latitude = 16.180962
+}
---
+++ Places/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Adrian on 15.09.2016.
+//  Copyright © 2016 Adrian Kubała. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
