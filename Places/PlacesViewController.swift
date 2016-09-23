973070e01352e363c963ae9f3bafaa0f96a498e6
Adrian <adrian.kubala@nextisgreat.com>
2016-09-23 12:18:17 +0200

    Moved protocols' methods implementations to the VC class

--- Places/PlacesViewController.swift
+++ Places/PlacesViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import GooglePlaces
 import MapKit
 
-class PlacesViewController: UIViewController {
+class PlacesViewController: UIViewController, CLLocationManagerDelegate, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var placesView: UITableView!
@@ -23,6 +23,8 @@ class PlacesViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        reloadTable()
         setupLocationManager()
         setupTableView()
         setupSearchBar()
@@ -37,15 +39,106 @@ class PlacesViewController: UIViewController {
         locationManager.startUpdatingLocation()
     }
     
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        guard let userLocation = location else {
+            return
+        }
+        
+        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: center, span: span)
+        
+        mapView.setRegion(region, animated: true)
+        
+        setupMapView()
+        
+        let point = MKPointAnnotation()
+        
+        point.coordinate = userLocation.coordinate
+        point.title = "Current location"
+        
+        mapView.addAnnotation(point)
+        
+        locationManager.stopUpdatingLocation()
+    }
+    
     func setupTableView() {
         placesView.delegate = self
         placesView.dataSource = self
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searchIsActive() {
+            return filteredPlaces.count
+        }
+        return places.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("place", forIndexPath: indexPath)
+        
+        let row = indexPath.row
+        let data = chooseData(row)
+        
+        cell.textLabel?.text = data.name
+        if data.distance > 0 {
+            cell.detailTextLabel?.text = String(data.distance) + " m" + " | " + data.address
+        } else {
+            cell.detailTextLabel?.text = data.address
+        }
+        
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("selected")
     }
     
     func setupSearchBar() {
         searchBar.autocapitalizationType = .None
         searchBar.placeholder = "Current location"
         searchBar.delegate = self
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: #selector(PlacesViewController.makeRequestForPlaces), userInfo: nil, repeats: true)
+    }
+    
+    func makeRequestForPlaces() {
+        guard let searchText = searchBar.text else {
+            return
+        }
+        
+        let placesClient = GMSPlacesClient()
+        let filter = GMSAutocompleteFilter()
+        filter.country = "PL"
+        let center = locationManager.location?.coordinate
+        let northEast = CLLocationCoordinate2DMake(center!.latitude + 0.001, center!.longitude + 0.001)
+        let southWest = CLLocationCoordinate2DMake(center!.latitude - 0.001, center!.longitude - 0.001)
+        let viewport = GMSCoordinateBounds(coordinate: northEast, coordinate: southWest)
+        //        let config = GMSPlacePickerConfig(viewport: viewport)
+        //        let placePicker = GMSPlacePicker(config: config)
+        placesClient.autocompleteQuery(searchText, bounds: viewport, filter: filter, callback: { (predictions, error) -> Void in
+            guard let predictions = predictions else {
+                return
+            }
+            
+            var predicatedPlaces: [Place] = []
+            for prediction in predictions {
+                let placeName = prediction.attributedPrimaryText.string
+                let placeSubname = prediction.attributedSecondaryText?.string
+                if let subname = placeSubname {
+                    predicatedPlaces.append(Place(name: placeName, address: subname))
+                } else {
+                    predicatedPlaces.append(Place(name: placeName, address: ""))
+                }
+            }
+            
+            self.filteredPlaces = predicatedPlaces
+            self.reloadTable()
+        })
     }
     
     func showNearbyPlaces() {
@@ -74,12 +167,6 @@ class PlacesViewController: UIViewController {
                 self.placesView.reloadData()
             }
         })
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        reloadTable()
     }
     
     func reloadTable() {
@@ -121,108 +208,5 @@ class PlacesViewController: UIViewController {
     
     func searchIsActive() -> Bool {
         return searchBar.text?.isEmpty == false ? true : false
-    }
-}
-
-extension PlacesViewController: CLLocationManagerDelegate {
-    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let location = locations.last
-        guard let userLocation = location else {
-            return
-        }
-        
-        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
-        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-        let region = MKCoordinateRegion(center: center, span: span)
-        
-        mapView.setRegion(region, animated: true)
-        
-        setupMapView()
-        
-        let point = MKPointAnnotation()
-        
-        point.coordinate = userLocation.coordinate
-        point.title = "Current location"
-        
-        mapView.addAnnotation(point)
-        
-        locationManager.stopUpdatingLocation()
-    }
-}
-
-extension PlacesViewController: UITableViewDelegate {
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-}
-
-extension PlacesViewController: UITableViewDataSource {
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if searchIsActive() {
-            return filteredPlaces.count
-        }
-        return places.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("place", forIndexPath: indexPath)
-        
-        let row = indexPath.row
-        let data = chooseData(row)
-        
-        cell.textLabel?.text = data.name
-        if data.distance > 0 {
-            cell.detailTextLabel?.text = String(data.distance) + " m" + " | " + data.address
-        } else {
-            cell.detailTextLabel?.text = data.address
-        }
-        
-        
-        return cell
-    }
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        print("selected")
-    }
-}
-
-extension PlacesViewController: UISearchBarDelegate {
-    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
-        NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: #selector(PlacesViewController.makeRequestForPlaces), userInfo: nil, repeats: true)
-    }
-    
-    func makeRequestForPlaces() {
-        guard let searchText = searchBar.text else {
-            return
-        }
-        
-        let placesClient = GMSPlacesClient()
-        let filter = GMSAutocompleteFilter()
-        filter.country = "PL"
-        let center = locationManager.location?.coordinate
-        let northEast = CLLocationCoordinate2DMake(center!.latitude + 0.001, center!.longitude + 0.001)
-        let southWest = CLLocationCoordinate2DMake(center!.latitude - 0.001, center!.longitude - 0.001)
-        let viewport = GMSCoordinateBounds(coordinate: northEast, coordinate: southWest)
-        //        let config = GMSPlacePickerConfig(viewport: viewport)
-        //        let placePicker = GMSPlacePicker(config: config)
-        placesClient.autocompleteQuery(searchText, bounds: viewport, filter: filter, callback: { (predictions, error) -> Void in
-            guard let predictions = predictions else {
-                return
-            }
-            
-            var predicatedPlaces: [Place] = []
-            for prediction in predictions {
-                let placeName = prediction.attributedPrimaryText.string
-                let placeSubname = prediction.attributedSecondaryText?.string
-                if let subname = placeSubname {
-                    predicatedPlaces.append(Place(name: placeName, address: subname))
-                } else {
-                    predicatedPlaces.append(Place(name: placeName, address: ""))
-                }
-            }
-            
-            self.filteredPlaces = predicatedPlaces
-            self.reloadTable()
-        })
     }
 }
