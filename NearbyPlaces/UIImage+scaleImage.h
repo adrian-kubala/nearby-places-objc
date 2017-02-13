c53f7c4674fe1313954935c12d4cd6a771529074
Adrian Kubała <adrian.kubala@gmail.com>
2017-02-14 00:27:46 +0100

    Implement class

---
+++ NearbyPlaces/UIImage+scaleImage.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+scaleImage.h
+//  NearbyPlaces
+//
+//  Created by Adrian Kubała on 14.02.2017.
+//  Copyright © 2017 Adrian Kubała. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (scaleImage)
+
+- (instancetype)scaleImage:(double)width;
+
+@end
