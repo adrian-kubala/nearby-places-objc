757938043c7ec88d49911d8203bb8726855fe455
Adrian Kubała <adrian.kubala@gmail.com>
2017-02-14 23:32:19 +0100

    Declare class and public properties

---
+++ NearbyPlaces/CustomView.h
@@ -0,0 +1,17 @@
+//
+//  CustomView.h
+//  NearbyPlaces
+//
+//  Created by Adrian Kubała on 14.02.2017.
+//  Copyright © 2017 Adrian Kubała. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+NS_ASSUME_NONNULL_BEGIN
+@interface CustomView : NSObject
+
+- (instancetype)initWithView:(UIView *)view;
+- (void)drawViewByBezierPathWithOffset:(CGFloat)leftBottomCornerOffset color:(UIColor *)color;
+
+@end
+NS_ASSUME_NONNULL_END
---
+++ NearbyPlaces/CustomView.m
@@ -0,0 +1,13 @@
+//
+//  CustomView.m
+//  NearbyPlaces
+//
+//  Created by Adrian Kubała on 14.02.2017.
+//  Copyright © 2017 Adrian Kubała. All rights reserved.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
+@end
