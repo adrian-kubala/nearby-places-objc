cdbb7212f4e03659ee8188b9a5f8aaa917824999
Adrian Kubała <adrian.kubala@gmail.com>
2017-02-14 21:22:21 +0100

    Implement methods

--- NearbyPlaces/Place.m
+++ NearbyPlaces/Place.m
@@ -10,4 +10,30 @@
 
 @implementation Place
 
+-(instancetype)initWithName:(NSString *)name
+                    address:(NSString *)address
+                 coordinate:(CLLocationCoordinate2D)coordinate
+                      photo:(UIImage *)photo
+               userLocation:(CLLocationCoordinate2D)userLocation {
+  self = [super init];
+  
+  if (self) {
+    _name = name;
+    _address = address;
+    _coordinate = coordinate;
+    _photo = photo;
+    _distance = [self getDistanceFrom:coordinate to:userLocation];
+  }
+  
+  return self;
+}
+
+-(NSInteger)getDistanceFrom:(CLLocationCoordinate2D)place to:(CLLocationCoordinate2D)otherPlace {
+  CLLocation *firstLocation = [[CLLocation alloc] initWithLatitude:place.latitude longitude:place.longitude];
+  CLLocation *secondLocation = [[CLLocation alloc] initWithLatitude:otherPlace.latitude longitude:otherPlace.longitude];
+  
+  int distanceInMeters = [secondLocation distanceFromLocation:firstLocation];
+  return distanceInMeters;
+}
+
 @end
